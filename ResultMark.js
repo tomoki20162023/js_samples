dc8878cbb6ae261fb6be3711f98be15e37dc262f
tomoki162023 <tmk.ggl81@gmail.com>
2023-10-03 07:11:58 +0900

    Update ResultMark.js

--- ResultMark.js
+++ ResultMark.js
@@ -4,7 +4,9 @@ function ResultMark(_ok, _ng) {
 }
 ResultMark.prototype.OK = function() { return this._ok; };
 ResultMark.prototype.NG = function() { return this._ng; };
-ResultMark.prototype.judge = function(arg) {
-	return (arg ? this._ok : this._ng);
+ResultMark.prototype.judge = function(result) {
+	return (result ? this._ok : this._ng);
 };
+ResultMark.prototype.setOK = function(aChar) { this._ok = aChar; };
+ResultMark.prototype.setNG = function(aChar) { this._ng = aChar; };
 
