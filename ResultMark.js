55e86d60ada398d1f78e1917037c1cb775e9d470
tomoki162023 <tmk.ggl81@gmail.com>
2023-10-03 06:50:39 +0900

    add judge

--- ResultMark.js
+++ ResultMark.js
@@ -4,4 +4,7 @@ function ResultMark(_ok, _ng) {
 }
 ResultMark.prototype.OK = function() { return this._ok; };
 ResultMark.prototype.NG = function() { return this._ng; };
+ResultMark.prototype.judge = function(arg) {
+	return (arg ? this._ok : this._ng);
+};
 
